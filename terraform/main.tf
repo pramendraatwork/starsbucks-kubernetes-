31010d964cecec1ea35214bf9473d228bcc7cdc2
pramendraatwork <pramendratwork@gmail.com>
2026-02-26 00:15:05 +0530

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,43 +9,44 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# create security group for the ec2 instance
+# Create Security Group
 resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 22"
+  name        = "ec2-security-group"
+  description = "Allow SSH access on port 22"
 
-  # allow access on port 22
   ingress {
-    description = "ssh access"
+    description = "SSH access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Change to your IP for better security
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Monitoring server security group"
+    Name = "Monitoring-server-security-group"
   }
 }
 
-resource "aws_instance" "Monitoring_server" {
-ami = "ami-00bb6a80f01f03502"  
-instance_type = "t2.medium"
-security_groups = [aws_security_group.ec2_security_group.name]
-key_name = var.key_name
-tags = {
-  Name: var.instance_name
-}
+# Create EC2 Instance
+resource "aws_instance" "monitoring_server" {
+  ami                    = "ami-019715e0d74f695be"
+  instance_type          = "c7i-flex.large"
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = var.instance_name
+  }
 }
